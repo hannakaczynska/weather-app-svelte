4e7a93c8070f1c0150e379d14a62693c862f5b24
hannakaczynska <hannakacz13@gmail.com>
2026-03-03 16:14:03 +0100

    fetch 7 day forecast

--- src/lib/services/weather.ts
+++ src/lib/services/weather.ts
@@ -1,12 +1,12 @@
-import type { CurrentWeatherResult } from "$lib/types/weather";
+import type { WeatherResult} from "$lib/types/weather";
 
-export const getCurrentWeather = async (
+export const getWeather = async (
   latitude: number,
   longitude: number,
-): Promise<CurrentWeatherResult | undefined> => {
+): Promise<WeatherResult | undefined> => {
   try {
     const response = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m,relative_humidity_2m,weather_code&timezone=auto`,
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m,relative_humidity_2m,weather_code&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=auto`,
     );
 
     if (!response.ok) {
@@ -14,12 +14,22 @@ export const getCurrentWeather = async (
     }
 
     const data = await response.json();
-    return {
+
+    const currentWeather = {
       temperature: data.current.temperature_2m,
       wind: data.current.wind_speed_10m,
       humidity: data.current.relative_humidity_2m,
       code: data.current.weather_code,
     };
+
+    const forcast = {
+      maxTemperatures: data.daily.temperature_2m_max,
+      minTemperatures: data.daily.temperature_2m_min,
+      dates: data.daily.time,
+      codes: data.daily.weather_code,
+    };
+
+    return {currentWeather, forcast};
   } catch (err) {
     console.error(err);
   }
--- src/lib/types/weather.ts
+++ src/lib/types/weather.ts
@@ -4,3 +4,15 @@ wind: number,
 humidity: number,
 code: number
 }
+
+export interface ForcastResult {
+    maxTemperatures: number[],
+    minTemperatures: number[],
+    dates: string[],
+    codes: number[]
+}
+
+export interface WeatherResult {
+    currentWeather: CurrentWeatherResult,
+    forcast: ForcastResult,
+}
