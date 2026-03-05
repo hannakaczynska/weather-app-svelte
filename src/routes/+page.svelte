814816b784b79ea651fdcddcde5322f7479e3513
hannakaczynska <hannakacz13@gmail.com>
2026-03-05 10:17:32 +0100

    fetching weather in the global store

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,11 @@
 <script>
-  import { getWeather } from "$lib/services/weather";
-  import { getGeo } from "$lib/services/geocoding";
+  import { store } from "$lib/stores/store.svelte";
   import WeatherCard from "$lib/components/WeatherCard.svelte";
   import WeatherLocation from "$lib/components/WeatherLocation.svelte";
   import CurrentWeather from "$lib/components/CurrentWeather.svelte";
   import ForecastWeather from "$lib/components/ForecastWeather.svelte";
 
-//   console.log(getWeather(52.23009, 21.017075));
-//   console.log(getGeo("Berlin"));
-//   getGeo("Grodków");
+$inspect(store);
 </script>
 
 <WeatherCard>
