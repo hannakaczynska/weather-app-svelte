0db6976ee28dcce760e6aecfba26441a9ea8a8df
hannakaczynska <hannakacz13@gmail.com>
2026-03-05 11:54:28 +0100

    styling logo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header class="w-screen bg-[#AFD0DB] flex flex-col justify-center items-center gap-[20px] py-[20px] shadow-xl shadow-zinc-500/50 ">
-  <span>WeatherApp</span>
+  <span class="font-logo text-2xl md:text-3xl">WeatherApp</span>
   <SearchInput />
 </header>
 
