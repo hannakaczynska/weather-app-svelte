c85c4f678d169ba2128c575d5ecffe181bb77bb1
hannakaczynska <hannakacz13@gmail.com>
2026-03-06 20:07:44 +0100

    add hello message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,12 +11,20 @@
   <div class="w-full">
     <h2 class="text-center mt-[50px]">Loading...</h2>
   </div>
-{:else}
+{:else if store.weather && store.currentCity}
 <div class="w-full flex flex-col gap-[30px]">
   <h2>Weather</h2>
   <WeatherLocation />
   <CurrentWeather />
   <ForecastWeather />
 </div>
+{:else}
+  <div class="w-full text-center">
+    <h2 class="text-2xl font-semibold mt-[50px] mb-[20px]">Welcome to WeatherApp</h2>
+    <p class="text-gray-600 mb-[10px]">Get current weather and 7-day forecasts</p>
+    <p class="text-gray-600">Use the search bar above to find your city</p>
+  </div>
 {/if}
 </WeatherCard>
+
+
